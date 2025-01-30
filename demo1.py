7686e0f45a6e44036628d672ec1f75f0da979810
Romellosmooth <romello1781@gmail.com>
2025-01-30 17:53:12 -0500

    one lined added also added another variable and defined a function

--- demo1.py
+++ demo1.py
@@ -1,2 +1,9 @@
 print("hello")
 print("Welcome to Romello")
+Kayla = "Beautiful"
+print (Kayla)
+def say_hello(name):
+    return f"Hello, {name}!"
+
+# Call the function and print the result
+print(say_hello("Omar"))
